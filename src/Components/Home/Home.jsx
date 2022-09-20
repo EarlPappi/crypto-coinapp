f35e18b92a31be4d4c969954aa5c2652eb91b1f0
earlpappi <earlpappi@gmail.com>
2022-09-20 09:30:35 +0100

    demo custom hook

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,18 @@ const Home = () => {
     return (
         <div>
             {loading && <h2>Data is Loading</h2>}
-           <p>Data Length { data.length }</p>
+           { data && data.map(data =>{
+               return (
+                   <div style={{
+                       padding: '2em',
+                       
+                   }}>
+                       <h2>{ data.title }</h2>
+                       <p>{ data.content }</p>
+                       <h4>Written By: { data.author }</h4>
+                   </div>
+               )
+           }) }
             {error && <p>{ error }</p>}
         </div>
     );
--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,23 +1,51 @@
-import { useReducer } from "react"
+import axios from "axios";
+import { useEffect, useReducer } from "react"
 
-const ACTIONS ={
+const ACTIONS = {
     API_REQUEST: 'api-request',
     FETCH_DATA: 'fetch-data',
     ERROR: 'error'
 }
 
-function reducer(state, { type, payload }){
-    
-}
-
 const initialState = {
     data: [],
-    loading: true,
+    loading: false,
     error: null
 }
 
-export function useFetch(url){
-    const [state, dispatch] = useReducer(reducer, initialState)
+function reducer(state, { type, payload }) {
+    console.log(payload)
+
+    switch (type) {
+        case ACTIONS.API_REQUEST:
+            return {...state, data: [], loading: true};
+        case ACTIONS.FETCH_DATA:
+            return {...state, data: payload.data, loading: false};
+        case ACTIONS.ERROR:
+            return {...state, data: [], loading: false, error: payload};
+            
+           
+    
+        default:
+            return state;
+    }
+}
+
+
+
+export function useFetch(url) {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        dispatch({ type: ACTIONS.API_REQUEST });
+        axios.get(url)
+            .then(res => {
+                dispatch({ type: ACTIONS.FETCH_DATA, payload: res.data })
+            })
+            .catch((e) => {
+                dispatch({ type: ACTIONS.ERROR, payload: e.error })
+            })
+    }, [url])
 
 
     return state;
