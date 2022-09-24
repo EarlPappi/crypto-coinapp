45ceb72558dfe5559f67a30b24a42ea037059f07
earlpappi <earlpappi@gmail.com>
2022-09-24 15:00:56 +0100

    --commit Okay, dashboard started

--- src/Components/Directory/Dashboard/Dashboard.jsx
+++ src/Components/Directory/Dashboard/Dashboard.jsx
@@ -1,9 +1,24 @@
-
+import classes from './Dashboard.module.css'
 
 const Dashboard = () => {
     return ( 
-        <div>
-            <h1>Dashboard</h1>
+        <div className='p-8'>
+            <div className='flex gap-[1em]'>
+                <div className={`${classes.port1} flex-1 p-8 rounded-[1em]`}>
+                    <h1 className='text-center text-2xl text-white font-bold'>$18,000</h1>
+                    <span>Bitcoin</span>
+                </div>
+
+                <div className={`${classes.port1} flex-1 p-8 rounded-[1em]`}>
+                    <h1 className='text-center text-2xl text-white font-bold'>$18,000</h1>
+                    <span>Bitcoin</span>
+                </div>
+
+                <div className={`${classes.port1} flex-1 p-8 rounded-[1em]`}>
+                    <h1 className='text-center text-2xl text-white font-bold'>$18,000</h1>
+                    <span>Bitcoin</span>
+                </div>
+            </div>
         </div>
      );
 }
